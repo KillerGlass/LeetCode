1ff148d061a28ab6043bbebae658e38c1ddd1d91
killer glass <killerglass231@gmail.com>
2022-08-05 13:10:52 -0300

    logo

---
+++ Python/234. Palindrome Linked List.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        header = []
+        while (head):
+            header.append(head.val)
+            head = head.next
+
+        return header == header[::-1]
