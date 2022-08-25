eb4463e821011bc16457c2cbc9085b37fef10035
killer glass <killerglass231@gmail.com>
2022-08-25 11:55:14 -0300

    easy demais

--- Python/27. Remove Element.py
+++ Python/27. Remove Element.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: list[int], val: int) -> int:
         while val in nums:
             nums.remove(val)
 
